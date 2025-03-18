6ae26f44484ca07e8fa6c8a6fd31f64162bb77df
Titanium2099 <yasgh@protonmail.com>
2025-03-18 10:50:51 -0700

    V2 -> V3

--- src/extensions/tw/index.js
+++ src/extensions/tw/index.js
@@ -25,7 +25,7 @@ class TurboWarpBlocks {
     getInfo () {
         return {
             id: 'tw',
-            name: 'TurboWarp',
+            name: 'Advanced Blocks',
             color1: '#ff4c4c',
             color2: '#e64444',
             color3: '#c73a3a',
